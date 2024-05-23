86dffdc8300efbcfa41c3edd5b280c5ac368da8c
sqrtox <84203131+sqrtox@users.noreply.github.com>
2024-05-23 23:05:11 +0900

    マップ読み込みダイアログの修正

--- src/components/load-map-dialog.tsx
+++ src/components/load-map-dialog.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useState } from "react";
 import { create } from "zustand";
 import Dialog from "@mui/material/Dialog";
+import Stack from "@mui/material/Stack";
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogActions from "@mui/material/DialogActions";
@@ -8,6 +9,9 @@ import TextField from "@mui/material/TextField";
 import { useRPGMapStore } from "./app";
 import Button from "@mui/material/Button";
 import { RPGMap } from "@/features/rpgen/utils/map";
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { decompressFromEncodedURIComponent } from "lz-string";
 
 export type LoadMapDialogStore = {
   open: boolean,
@@ -31,6 +35,7 @@ const PLACEHOLDER = `\
 
 export default function LoadMapDialog(): ReactNode {
   const { open, setOpen } = useLoadMapDialogStore();
+  const [loadCompressed, setLoadCompressed] = useState(false);
   const { setRPGMap } = useRPGMapStore();
   const [disabled, setDisabled] = useState(false);
   const [mapData, setMapData] = useState("");
@@ -42,23 +47,38 @@ export default function LoadMapDialog(): ReactNode {
     >
       <DialogTitle>マップを読み込む</DialogTitle>
       <DialogContent>
-        <TextField
-          sx={{
-            width: "500px"
-          }}
-          multiline
-          rows={5}
-          placeholder={PLACEHOLDER}
-          value={mapData}
-          onChange={event => setMapData(event.currentTarget.value)}
-        />
+        <Stack spacing={1}>
+          <FormControlLabel
+            control={<Checkbox checked={loadCompressed} />}
+            label="圧縮されたマップを読み込む"
+            onChange={(_event, checked) => setLoadCompressed(checked)}
+          />
+          <TextField
+            sx={{
+              width: "500px"
+            }}
+            multiline
+            rows={5}
+            placeholder={loadCompressed ? COMPRESSED_PLACEHOLDER : PLACEHOLDER}
+            value={mapData}
+            onChange={event => setMapData(event.currentTarget.value)}
+          />
+        </Stack>
       </DialogContent>
       <DialogActions>
         <Button
           disabled={disabled}
           onClick={() => {
             setDisabled(true);
-            setRPGMap(RPGMap.parse(mapData));
+
+            const actualMapData = loadCompressed
+              ? decompressFromEncodedURIComponent(mapData.replace(/^L1/, ""))
+              : mapData;
+
+            console.log(actualMapData);
+
+            setRPGMap(RPGMap.parse(actualMapData));
+            setDisabled(false);
             setOpen(false);
           }}
         >
