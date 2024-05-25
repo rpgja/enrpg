c861d8730365c8ef584dc2f94a7b828d705efaa1
sqrtox <84203131+sqrtox@users.noreply.github.com>
2024-05-25 14:32:54 +0900

    レイヤー草案

--- src/features/editor/components/menu-bar.tsx
+++ src/features/editor/components/menu-bar.tsx
@@ -1,15 +1,48 @@
 import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Slider from "@mui/material/Slider";
+import Stack from "@mui/material/Stack";
 import Toolbar from "@mui/material/Toolbar";
-import type { ReactNode } from "react";
+import { type ReactNode, useState } from "react";
 import DisplayMenu from "./display-menu";
+import { useEditorStore } from "./editor-view";
 import FileMenu from "./file-menu";
 
 export default function MenuBar(): ReactNode {
+  const editor = useEditorStore((store) => store.editor);
+  const [layerLevel, setLayerLevel] = useState(10);
+
   return (
     <AppBar sx={{ backgroundColor: "background.paper" }} position="static">
       <Toolbar variant="dense" disableGutters sx={{ minHeight: 0 }}>
-        <FileMenu />
-        <DisplayMenu />
+        <Stack direction="row" spacing={4}>
+          <Stack direction="row">
+            <FileMenu />
+            <DisplayMenu />
+          </Stack>
+          <FormControlLabel
+            label="レイヤー"
+            labelPlacement="start"
+            control={
+              <Slider
+                min={0}
+                max={10}
+                value={layerLevel}
+                size="small"
+                sx={{ width: "10rem", marginLeft: "1rem" }}
+                onChange={(_event, value) => {
+                  if (typeof value !== "number") {
+                    return;
+                  }
+
+                  setLayerLevel(value);
+                  editor?.renderer.setLayerLevel(value);
+                }}
+              />
+            }
+          />
+        </Stack>
       </Toolbar>
     </AppBar>
   );
