6da1a2266308c10e85777799fa81b30bacd22f83
Feederkyota <kyoooooooooota>
2024-06-07 08:04:00 +0900

    ワイヤーフレーム作成

--- src/features/editor/components/tile-palette-window.tsx
+++ src/features/editor/components/tile-palette-window.tsx
@@ -8,6 +8,7 @@ import Box from "@mui/material/Box";
 import FormControl from "@mui/material/FormControl";
 import IconButton from "@mui/material/IconButton";
 import InputLabel from "@mui/material/InputLabel";
+import ListSubheader from "@mui/material/ListSubheader";
 import MenuItem from "@mui/material/MenuItem";
 import Paper from "@mui/material/Paper";
 import Select from "@mui/material/Select";
@@ -93,12 +94,46 @@ export default function TilePaletteWindow({
               arrayFrom,
             )}
           </TabList>
+          <TabPanel value={TilePaletteTab.Mylist} sx={{ flex: 1 }}>
+            <Stack spacing={2} height="100%">
+              <FormControl fullWidth>
+                <InputLabel>種類</InputLabel>
+                <Select value="human" label="種類">
+                  <MenuItem value="human">NPC</MenuItem>
+                  <MenuItem value="any">タイル</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="grouped-select">マイリスト</InputLabel>
+                <Select defaultValue="" id="grouped-select" label="Grouping">
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <ListSubheader>お城</ListSubheader>
+                  <MenuItem value={1}>姫路城</MenuItem>
+                  <MenuItem value={2}>安土城</MenuItem>
+                  <MenuItem value={3}>ダークパレス</MenuItem>
+                  <ListSubheader>可愛いキャラ</ListSubheader>
+                  <MenuItem value={4}>動物セット</MenuItem>
+                  <MenuItem value={5}>人間セット</MenuItem>
+                </Select>
+              </FormControl>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <TextField fullWidth size="small" label="マイリスト検索" />
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </Stack>
+              <Paper sx={{ flex: 1 }} />
+            </Stack>
+          </TabPanel>
           <TabPanel value={TilePaletteTab.Custom} sx={{ flex: 1 }}>
             <Stack spacing={2} height="100%">
               <FormControl fullWidth>
                 <InputLabel>種類</InputLabel>
                 <Select value="human" label="種類">
-                  <MenuItem value="human">人</MenuItem>
+                  <MenuItem value="human">NPC</MenuItem>
+                  <MenuItem value="any">タイル</MenuItem>
                 </Select>
               </FormControl>
               <Stack direction="row" spacing={1} alignItems="center">
@@ -108,6 +143,51 @@ export default function TilePaletteWindow({
                 </IconButton>
               </Stack>
               <Paper sx={{ flex: 1 }} />
+            </Stack>
+          </TabPanel>
+          <TabPanel value={TilePaletteTab.DQ} sx={{ flex: 1 }}>
+            <Stack spacing={2} height="100%">
+              <FormControl fullWidth>
+                <InputLabel>種類</InputLabel>
+                <Select value="human" label="種類">
+                  <MenuItem value="human">NPC</MenuItem>
+                  <MenuItem value="any">タイル</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl fullWidth>
+                <InputLabel>初期素材</InputLabel>
+                <Select value="human" label="種類">
+                  <MenuItem value="human">地面（歩ける）</MenuItem>
+                  <MenuItem value="any">地面（歩けない）</MenuItem>
+                  <MenuItem value="any">物（ぶつかる）</MenuItem>
+                  <MenuItem value="any">物（ぶつからない）</MenuItem>
+                  <MenuItem value="any">-----------</MenuItem>
+                  <MenuItem value="any">階段</MenuItem>
+                  <MenuItem value="any">建物</MenuItem>
+                  <MenuItem value="any">マップ移動ポイント</MenuItem>
+                  <MenuItem value="any">扉</MenuItem>
+                  <MenuItem value="any">宝箱</MenuItem>
+                  <MenuItem value="any">しらべるポイント</MenuItem>
+                  <MenuItem value="any">-----------</MenuItem>
+                  <MenuItem value="any">イベントポイント</MenuItem>
+                  <MenuItem value="any">-----------</MenuItem>
+                  <MenuItem value="any">掲示板/本/ネット</MenuItem>
+                </Select>
+              </FormControl>
+              <Paper sx={{ flex: 1 }} />
+            </Stack>
+          </TabPanel>
+          <TabPanel value={TilePaletteTab.History} sx={{ flex: 1 }}>
+            <Stack spacing={2} height="100%">
+              <FormControl fullWidth>
+                <InputLabel>種類</InputLabel>
+                <Select value="human" label="種類">
+                  <MenuItem value="human">NPC</MenuItem>
+                  <MenuItem value="any">タイル</MenuItem>
+                </Select>
+              </FormControl>
+              <Paper sx={{ flex: 1 }} />
+              <Box>ページネーション的なやつ</Box>
             </Stack>
           </TabPanel>
         </Stack>
